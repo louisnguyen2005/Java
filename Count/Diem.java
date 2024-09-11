65407fd30ac991d8c7f303bf8003297c10631d37
louisnguyen2005 <kinguyen291118@gmail.com>
2024-09-11 22:37:29 +0700

    first commit

---
+++ Count/Diem.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Diem {
+    private int x, y;
+
+    public Diem() {
+        x = y = 0;
+    }
+
+    public Diem(int x1, int y1) {
+        x = x1;
+        y = y1;
+    }
+
+    public Diem(int x2) {
+        x = x2;
+        y = 0;
+    }
+
+    public void gan(int x1, int y1) {
+        x = x1;
+        y = y1;
+    }
+
+    public void in() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap hoanh do:");
+        x = sc.nextInt();
+        System.out.println("nhap tung do:");
+        y = sc.nextInt();
+
+    }
+
+    public int layX() {
+        return x;
+    }
+
+    public int layY() {
+        return y;
+    }
+
+    public void tinhtien(int dx, int dy) {
+        dx += x;
+        dy += y;
+    }
+
+    public float distance() {
+        return (float) (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+    }
+
+    public float distanceAB(Diem B) {
+        return (float) (Math.sqrt(Math.pow(x - B.x, 2) + Math.pow(y - B.y, 2)));
+    }
+}
---
+++ Count/SDDiem.java
@@ -0,0 +1,15 @@
+public class SDDiem {
+    public static void main(String[] args) {
+        Diem A = new Diem(5, 7);
+        System.out.println("toa do cua A la:");
+        A.in();
+        Diem B = new Diem();
+        B.nhap();
+        B.in();
+        Diem C = new Diem(10);
+        Diem D = new Diem(-B.layX(), -B.layY());
+        System.out.println("Diem doi xung cua D la:");
+        D.in();
+        System.out.println("khoang cach tu B den O:" + B.distance());
+    }
+}
